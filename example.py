02264e7c940e940d63977f993846ad1e738d8cc6
Ahmad Abdellatif <124534399+notthatanonymous@users.noreply.github.com>
2023-10-18 17:36:28 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,19 +1,19 @@
-from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 import time
 
 from scitime import RuntimeEstimator
 
-# example for kmeans clustering
+# example for rf regressor
 estimator = RuntimeEstimator(meta_algo='RF', verbose=3)
-km = KMeans()
+rf = RandomForestRegressor()
 
-X = np.random.rand(100000,10)
+X,y = np.random.rand(100000,10),np.random.rand(100000,1)
 # run the estimation
-estimation, lower_bound, upper_bound = estimator.time(km, X)
+estimation, lower_bound, upper_bound = estimator.time(rf, X, y)
 
 # compare to the actual training time
 start_time = time.time()
-km.fit(X)
+rf.fit(X,y)
 elapsed_time = time.time() - start_time
 print("elapsed time: {:.2}".format(elapsed_time))
